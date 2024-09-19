20808d245f45a922419719c031c68aa3a7814d61
NelsonAlvaradoAguilar <nelsonalvaradoaguilar@gmail.com>
2024-09-19 11:18:48 -0400

    test

--- src/Pages/HomePage/HomePage.js
+++ src/Pages/HomePage/HomePage.js
@@ -1,109 +1,89 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
-import {  apiUrl } from "../../ApiTools/KeyUrl";
+import { apiUrl } from "../../ApiTools/KeyUrl";
 import { useParams } from "react-router-dom";
 import DetailsContentPage from "../DetailsContentPage/DetailsContentPage";
 import VideoList from "../../components/VideoList/VideoList";
-import './HomePage.scss'
+import "./HomePage.scss";
 import Player from "../../components/Player/Player";
 
-
 export default function HomePage() {
+  const { videoId } = useParams();
+  const [sideLists, setSideList] = useState([]);
+  const [selectedVideo, setSelectedVideo] = useState({});
+  const [mainVideo, setMainVideo] = useState();
 
-    const { videoId } = useParams();
-    const [sideLists, setSideList] = useState([]);
-    const [selectedVideo, setSelectedVideo] = useState({});
-    const [mainVideo, setMainVideo] = useState()
+  useEffect(() => {
+    const id = videoId ? videoId : sideLists[0];
 
+    const getListVideo = async () => {
+      try {
+        const response = await axios.get(`${apiUrl}/videos`);
 
-
-    useEffect(() => {
-        const id = videoId ? videoId : sideLists[0];
-
-        const getListVideo = async () => {
-
-            try {
-                const response = await axios.get(`${apiUrl}/videos`);
-
-                setSideList(response.data)
-                setMainVideo(response.data[0])
-            } catch (error) {
-                console.log('this', error);
-            }
-
-        };
-
-
-        if (id) {
-            getListVideo();
-        }
-
-    }, [videoId]);
-
-
-
-    useEffect(() => {
-        const getVideoSelected = async () => {
-            try {
-
-                const response = await axios.get(`${apiUrl}/videos/${videoId}`);
-                setSelectedVideo(response.data);
-
-            } catch (error) {
-                console.log('this', error);
-            }
-
-        }
-        if (videoId) {
-            getVideoSelected();
-        };
-    }, [videoId]);
-
- 
-
-
-
-    const handleVideoSelect = () => {
-        const newdSideList = sideLists.filter(video => video.id !== selectedVideo.id);
-
-        setSideList(newdSideList)
-        return sideLists
-
+        setSideList(response.data);
+        setMainVideo(response.data[0]);
+      } catch (error) {
+        console.log("this", error);
+      }
     };
-    useEffect(() => {
 
-        if (selectedVideo) {
-            handleVideoSelect();
-            sideLists.push(selectedVideo);
-            
-        }
-    }, [selectedVideo]);
-
-
-
-
-
-    if (!videoId && !mainVideo) {
-        return <div>Loading...</div>;
+    if (id) {
+      getListVideo();
     }
+  }, [videoId]);
 
-    return (
-        <section className="page-container" >
+  useEffect(() => {
+    const getVideoSelected = async () => {
+      try {
+        const response = await axios.get(`${apiUrl}/videos/${videoId}`);
+        setSelectedVideo(response.data);
+      } catch (error) {
+        console.log("this", error);
+      }
+    };
+    if (videoId) {
+      getVideoSelected();
+    }
+  }, [videoId]);
 
+  const handleVideoSelect = () => {
+    const newdSideList = sideLists.filter(
+      (video) => video.id !== selectedVideo.id
+    );
 
-            {videoId ? <Player video={selectedVideo} /> : <Player video={mainVideo} />}
+    setSideList(newdSideList);
+    return sideLists;
+  };
+  useEffect(() => {
+    if (selectedVideo) {
+      handleVideoSelect();
+      sideLists.push(selectedVideo);
+    }
+  }, [selectedVideo]);
 
+  if (!videoId && !mainVideo) {
+    return <div>Loading...</div>;
+  }
 
+  return (
+    <section className="page-container">
+      {videoId ? (
+        <Player video={selectedVideo} />
+      ) : (
+        <Player video={mainVideo} />
+      )}
 
+      <div className="page-container__content">
+        {videoId && (
+          <DetailsContentPage
+            selectedVideo={selectedVideo}
+            sideLists={sideLists}
+            videoId={videoId}
+          />
+        )}
 
-            <div className="page-container__content">
-
-                {videoId && <DetailsContentPage selectedVideo={selectedVideo} sideLists={sideLists}  videoId={videoId}/>}
-
-                <VideoList  videos={sideLists} />
-            </div>
-
-        </section>
-    )
-
+        <VideoList videos={sideLists} />
+      </div>
+    </section>
+  );
 }
