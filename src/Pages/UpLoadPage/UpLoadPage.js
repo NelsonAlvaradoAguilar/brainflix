a028855a2274d480d4e50d1d4e8100f228829fad
NelsonAlvaradoAguilar <nelsonalvaradoaguilar@gmail.com>
2024-09-19 11:42:58 -0400

    url testing

--- src/Pages/UpLoadPage/UpLoadPage.js
+++ src/Pages/UpLoadPage/UpLoadPage.js
@@ -1,97 +1,105 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { apiUrl } from "../../ApiTools/KeyUrl";
-import './UpLoadPage.scss'
 
-
-
+import "./UpLoadPage.scss";
 
 export default function UpLoadPage(props) {
-    const [video, setVideo] = useState('');
-    const [title, setTitle] = useState('');
-    const [description, setdescription] = useState('');
+  const [video, setVideo] = useState("");
+  const [title, setTitle] = useState("");
+  const [description, setdescription] = useState("");
 
-   
+  const apiUrl = "https://nelson-alvarado-brainflix-api.vercel.app";
 
+  const handleChangeVideo = (e) => {
+    setVideo(e.target.value);
+  };
+  const handleChangeVideoTitle = (e) => {
+    setTitle(e.target.value);
+  };
 
-    const handleChangeVideo = (e) => {
-        setVideo(e.target.value);
-       
+  const handleChangeVideoDescription = (e) => {
+    setdescription(e.target.value);
+  };
+
+  const postVideos = async (video, title, description, image, channel) => {
+    try {
+      const response = await axios.post(`${apiUrl}/videos`, {
+        video,
+        title,
+        description,
+        image,
+        channel,
+      });
+      console.log(response.data);
+      return response.data;
+    } catch (error) {
+      console.log("this is the error", error);
     }
-    const handleChangeVideoTitle = (e) => {
-        setTitle(e.target.value);
-        
-    }
+  };
 
-    const handleChangeVideoDescription = (e) => {
-        setdescription(e.target.value);
-  
-    }
- 
+  const handlePostedVideo = async (e) => {
+    e.preventDefault();
+    const image = "http://localhost:8080/images/upload-video-preview.jpg";
 
-    const postVideos = async (video, title, description, image,channel) => {
+    const channel = "My First Video";
+    const resp = await postVideos(video, title, description, image, channel);
+    console.log(resp);
+  };
 
-        try {
-            const response = await axios.post(`${apiUrl}/videos`, { video, title, description, image,channel });
-            console.log(response.data);
-            return response.data;
-        } catch (error) {
-            console.log('this is the error', error);
-        }
-    }
-       
-        const handlePostedVideo = async (e) => {
-            e.preventDefault();
-            const image = 'http://localhost:8080/images/upload-video-preview.jpg';
-       
-           const channel='My First Video';
-            const resp = await postVideos(video, title, description, image,channel);
-            console.log(resp);
-        }
+  return (
+    <section className="upload-container">
+      <h1 className="upload-container__title">Upload Video</h1>
 
-
-    return (
-        <section className="upload-container">
-            <h1 className="upload-container__title">Upload Video</h1>
-
-            <div className="upload-container__content">
-
-
-
-                <form onSubmit={handlePostedVideo} className="upload-container__form">
-                    <div className="upload-container__thumbnail">
-                        <h2 className="upload-container__subtitle">VIDEO THUMBNAIL</h2>
-                        <input className="upload-container__player"
-                          type="file"
-                            value={video}
-                            onChange={handleChangeVideo}
-                        />
-
-                    </div>
-                    <label className="upload-container__subtitle">TITLE YOUR VIDEO   <br /><br />
-                        <input className="upload-container__input-field"
-                            placeholder="Add a title to your video"
-                            value={title}
-                            onChange={handleChangeVideoTitle}
-                            type="text"
-                        />
-                    </label>
-                    <br /><br />
-                    <label className="upload-container__subtitle">ADD A VIDEO DESCRIPTION <br /><br />
-                        <input className="upload-container__input-field upload-container__input-field--description"
-                            placeholder="Add a description to your video "
-                            value={description}
-                            onChange={handleChangeVideoDescription}
-                            type="text"
-                        />
-                    </label><br /><br />
-                    <div className="upload-container__buttons">
-                        <button type="submit" className="upload-container__button-publish" >PUBLISH</button><br /><br />
-                        <button className=" upload-container__button-cancel">CANCEL</button>
-                        <button type="submit"  className="upload-container__button-hiden" >PUBLISH</button>
-                    </div>
-                </form>
-            </div>
-        </section>
-    )
+      <div className="upload-container__content">
+        <form onSubmit={handlePostedVideo} className="upload-container__form">
+          <div className="upload-container__thumbnail">
+            <h2 className="upload-container__subtitle">VIDEO THUMBNAIL</h2>
+            <input
+              className="upload-container__player"
+              type="file"
+              value={video}
+              onChange={handleChangeVideo}
+            />
+          </div>
+          <label className="upload-container__subtitle">
+            TITLE YOUR VIDEO <br />
+            <br />
+            <input
+              className="upload-container__input-field"
+              placeholder="Add a title to your video"
+              value={title}
+              onChange={handleChangeVideoTitle}
+              type="text"
+            />
+          </label>
+          <br />
+          <br />
+          <label className="upload-container__subtitle">
+            ADD A VIDEO DESCRIPTION <br />
+            <br />
+            <input
+              className="upload-container__input-field upload-container__input-field--description"
+              placeholder="Add a description to your video "
+              value={description}
+              onChange={handleChangeVideoDescription}
+              type="text"
+            />
+          </label>
+          <br />
+          <br />
+          <div className="upload-container__buttons">
+            <button type="submit" className="upload-container__button-publish">
+              PUBLISH
+            </button>
+            <br />
+            <br />
+            <button className=" upload-container__button-cancel">CANCEL</button>
+            <button type="submit" className="upload-container__button-hiden">
+              PUBLISH
+            </button>
+          </div>
+        </form>
+      </div>
+    </section>
+  );
 }
